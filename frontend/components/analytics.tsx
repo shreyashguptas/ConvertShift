d4e14415a4fc6b9b4d1c39c8174755b637e5f2f9
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-12-03 14:24:38 -0600

    Getting dependencies fixed and making the upload button prominent

--- frontend/components/analytics.tsx
+++ frontend/components/analytics.tsx
@@ -4,4 +4,4 @@ import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 export function Analytics() {
   return <VercelAnalytics />;
-} 
+}
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig; 
